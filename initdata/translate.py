0c9626332794694f026e4fc2d0616815f307d42a
milan medlik <milan.medlik@wildfuse.com>
2011-12-17 21:09:24 +0100

    universal string for bada and android #1

---
+++ initdata/translate.py
@@ -0,0 +1,121 @@
+"package com.voc4u.lang.lesson%s\n// %s""package com.voc4u.lang.lesson%s\n// %s""\" //"#!/usr/bin/python
+
+
+import os
+import sys
+
+langs = [ "en" , "DE", "CZ"]
+types = ["android", "bada"]
+description = ["make xml files for android", "make xml files for bada"]
+actualtype = "java"
+temprow = {types[0] : "<string name=\"ID\">TEXT</string>", types[1] : "<text id=\"ID\">TEXT</text>"}
+template = { types[0] : "android.xml.template" , types[1] : "bada.xml.template"}
+keyToUpper = {types[0] : False, types[1] : True}
+keyPrefix = {types[0] : "", types[1] : "IDS_"}
+
+
+
+def showHelp(sys):
+    print "type:\n"
+    n = 0
+    for type in types:
+        print "\t" + type + " : " + description[n]
+        n += 1
+    
+    print "usage:"
+    print sys.argv[0] + " [--help] type [src_dir] [desc_dir]"
+
+
+
+
+def writeDesc(descpath, arr):
+    f = open(descpath, 'w')
+    
+    
+    temppath = "translates/" + template[actualtype]
+    if not os.path.exists(temppath) :
+        print "file : " + temppath + " IS NOT EXIST"
+        return ;
+    
+    tempfile = open(temppath, "r");
+    temp = tempfile.read();
+    temp = temp % {"arr" : arr}
+    
+#    fi = initfile[actualtype] % {"lesson" : str(l), "name" : la}
+#    fi += ";\n\npublic class Data" + la
+#    fi += "\n{\npublic static final String[] text = new String[]{\n" + arr
+#    fi += "\n};\n}"
+    
+    print >> f, temp
+    f.close()
+    tempfile.close()
+
+def makeFile(srcdir, descdir, la):
+    descpath = descdir + "/strings-" + actualtype + "-" + la + ".xml"
+    
+    print descpath
+	
+    
+    srcpath = "translates/" + la + ".csv"
+    if not os.path.exists(srcpath) :
+        print "file : " + srcpath + " IS NOT EXIST"
+        return ;
+    
+    
+     
+    arr = ""
+    count = 0
+    tempr = temprow[actualtype]
+    btoupper = keyToUpper[actualtype]
+    for line in open(srcpath, 'r').readlines():
+        lang = line.split(";");
+        
+        key = keyPrefix[actualtype] + lang[0]
+        
+        if btoupper:
+            key = key.upper()
+        else:
+            key = key.lower()
+        
+        text = ""
+        if len(lang)> 1:
+            for tnext in lang[1:-1]:
+                text += tnext
+                
+        tr = tempr.replace("ID", key).replace("TEXT", text)
+        
+        arr += "\n\t" + tr
+        
+    writeDesc(descpath, arr)
+
+def make(srcdir, descdir):
+    if descdir.endswith("/"):
+        descdir = descdir[0:-1]
+    if srcdir.endswith("/"):
+        srcdir = srcdir[0:-1]
+
+    for lang in langs:
+        makeFile(srcdir, descdir, lang)
+    
+
+dir = "."
+descdir = "."
+
+
+if len(sys.argv) < 2 or sys.argv[1] == "--help" or (sys.argv[1] != types[0] and sys.argv[1] != types[1] and sys.argv[1] != types[2] and sys.argv[1] != types[3]):
+    showHelp(sys)
+    sys.exit()
+
+actualtype = sys.argv[1]
+
+
+
+if len(sys.argv) > 2:
+	dir = sys.argv[2]
+if len(sys.argv) > 3:
+	descdir = sys.argv[3]
+
+if dir == "--help":
+    showHelp(sys)
+else:
+	make(dir, descdir)
